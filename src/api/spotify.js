989c3125a6d88a7a2dba71cf27580a425b8118fe
bjosefine <josefinebrolin95@gmail.com>
2023-02-22 15:25:32 +0100

    comments in spotify.js

--- src/api/spotify.js
+++ src/api/spotify.js
@@ -1,95 +1,95 @@
-const clientId = "424ae0b3a11c4659822d0aea1a2db86a";
-const clientSecret = "e9ee9f63754a4786b0b0a067253f7908";
-/// Get token from api 
+const clientId = '424ae0b3a11c4659822d0aea1a2db86a'
+const clientSecret = 'e9ee9f63754a4786b0b0a067253f7908'
+/// send client id & secret to api
 const getToken = async () => {
-  const response = await fetch("https://accounts.spotify.com/api/token", {
-    method: "POST",
+  const response = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
     headers: {
-      Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
-      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: 'Basic ' + btoa(clientId + ':' + clientSecret),
+      'Content-Type': 'application/x-www-form-urlencoded'
     },
-    body: "grant_type=client_credentials",
-  });
-
-  const data = await response.json();
+    body: 'grant_type=client_credentials'
+  })
+  /// retrive the token from api
+  const data = await response.json()
   console.log(data.access_token)
-  return data.access_token;
-};
-// Get  FeaturedPlaylists from api
+  return data.access_token
+}
+// Get  Featured Playlists from api
 const getFeaturedPlaylists = async () => {
-  const token = await getToken();
+  const token = await getToken()
   const response = await fetch(
-    "https://api.spotify.com/v1/browse/featured-playlists",
+    'https://api.spotify.com/v1/browse/featured-playlists',
     {
       headers: {
-        Authorization: "Bearer " + token,
-      },
+        Authorization: 'Bearer ' + token
+      }
     }
-  );
+  )
 
-  const data = await response.json();
-  return data.playlists.items;
-};
-
+  const data = await response.json()
+  return data.playlists.items
+}
+/// Get specific playlist from api
 const getPlaylist = async (playlistId) => {
-  const token = await getToken();
+  const token = await getToken()
   const response = await fetch(
     `https://api.spotify.com/v1/playlists/${playlistId}`,
     {
       headers: {
-        Authorization: `Bearer ${token}`,
-      },
+        Authorization: `Bearer ${token}`
+      }
     }
-  );
-  const data = await response.json();
-  return data;
-};
-
+  )
+  const data = await response.json()
+  return data
+}
+/// Get a list of song from playlist
 const getPlaylistTracks = async (playlistId) => {
-  const token = await getToken();
+  const token = await getToken()
   const response = await fetch(
     `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
     {
       headers: {
-        Authorization: `Bearer ${token}`,
-      },
+        Authorization: `Bearer ${token}`
+      }
     }
-  );
-  const data = await response.json();
-  return data.items;
-};
-
+  )
+  const data = await response.json()
+  return data.items
+}
+/// Get genres from api
 const getGenres = async () => {
-  const token = await getToken();
-  const response = await fetch("https://api.spotify.com/v1/browse/categories", {
+  const token = await getToken()
+  const response = await fetch('https://api.spotify.com/v1/browse/categories', {
     headers: {
-      Authorization: "Bearer " + token,
-    },
-  });
+      Authorization: 'Bearer ' + token
+    }
+  })
 
-  const data = await response.json();
-  return data.categories.items;
-};
-
+  const data = await response.json()
+  return data.categories.items
+}
+/// search tracks from api
 const searchTracks = async (query) => {
-  const token = await getToken();
+  const token = await getToken()
   const response = await fetch(
     `https://api.spotify.com/v1/search?q=${query}&type=track`,
     {
       headers: {
-        Authorization: "Bearer " + token,
-      },
+        Authorization: 'Bearer ' + token
+      }
     }
-  );
+  )
 
-  const data = await response.json();
-  return data.tracks.items;
-};
+  const data = await response.json()
+  return data.tracks.items
+}
 
 export default {
   getFeaturedPlaylists,
   getPlaylist,
   getPlaylistTracks,
   searchTracks,
-  getGenres,
-};
+  getGenres
+}
--- src/components/Playlist.vue
+++ src/components/Playlist.vue
@@ -1,37 +1,33 @@
 <template>
-    <div class="playlist" v-if="playlist">
-      <h1>{{ playlist.name }}</h1>
-      <div v-if="tracks">
-        <div v-for="track in tracks" :key="track.track.id">
-          {{ track.track.name }}
-        </div>
-      </div>
-      <div v-else>
-        Loading...
+  <div class="playlist" v-if="playlist">
+    <h1>{{ playlist.name }}</h1>
+    <div v-if="tracks">
+      <div v-for="track in tracks" :key="track.track.id">
+        {{ track.track.name }}
       </div>
     </div>
-  </template>
-  
-  <script>
-  import spotify from "../api/spotify.js";
-  
+    <div v-else>Loading...</div>
+  </div>
+</template>
+
+<script>
+  import spotify from '../api/spotify.js'
+
   export default {
-    name: "Playlist",
+    name: 'Playlist',
     data() {
       return {
         playlist: null,
-        tracks: null,
-      };
+        tracks: null
+      }
     },
     async created() {
-        const playlistId = this.$route.params.id;
-        this.playlist = await spotify.getPlaylist(playlistId);
-  this.tracks = await spotify.getPlaylistTracks(playlistId);
-  console.log(this.tracks.images.url)
+      const playlistId = this.$route.params.id
+      this.playlist = await spotify.getPlaylist(playlistId)
+      this.tracks = await spotify.getPlaylistTracks(playlistId)
     },
-    catch (error) {
-      console.error(error);
-    },
-  };
-  </script>
-  
+    catch(error) {
+      console.error(error)
+    }
+  }
+</script>
