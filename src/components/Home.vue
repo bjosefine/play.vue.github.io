d6025ede1cfd2f8bccc127683d9045447a903626
Malin Almqvist <malin.therese.almqvist@gmail.com>
2023-02-23 09:51:38 +0100

    fixed bug in home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,13 +2,12 @@
   <div class="home">
     <h1>Playlists</h1>
     <div v-if="featuredPlaylists">
-      <div
-        v-for="playlist in featuredPlaylists"
-        :key="(playlist.id, playlist.images)"
-      >
+      <div v-for="playlist in featuredPlaylists" :key="playlist.id">
         <router-link :to="{ name: 'playlist', params: { id: playlist.id } }">
           {{ playlist.name }}
-          <img :src="playlist.id.images[0].url" alt="" />
+          <!-- following code line gets images for featured playlists -->
+          <img :src="playlist.images[0].url" alt="" />
+          <!-- end of code -->
         </router-link>
       </div>
     </div>
