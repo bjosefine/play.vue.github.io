557b068ee6e53175c7651ba032649faf14af99a9
bjosefine <josefinebrolin95@gmail.com>
2023-02-24 16:46:46 +0100

    player view background image

--- src/views/PlayerView.vue
+++ src/views/PlayerView.vue
@@ -1,9 +1,16 @@
 <template>
+  <div
+    id="songBG"
+    :style="{
+      backgroundImage: 'url(' + img + ')',
+      filter: 'blur(4px)'
+    }"
+  ></div>
   <p>songs</p>
   <div v-if="tracks">
     <div>
       {{ tracks.name }}
-      {{ tracks.preview_url }}
+      <img :src="tracks.album.images[0].url" alt="bild" style="width: 50%" />
       <audio controls>
         <source :src="tracks.preview_url" type="audio/mp3" />
       </audio>
@@ -11,18 +18,29 @@
   </div>
   <div v-else>Loading...</div>
 </template>
+
+<style>
+  #songBG {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+</style>
+
 <script>
   import spotify from '../api/spotify'
 
   export default {
     data() {
       return {
-        tracks: null
+        tracks: null,
+        img: ''
       }
     },
     async created() {
       const songId = this.$route.params.id
       this.tracks = await spotify.getTracks(songId)
+      this.img = this.tracks.album.images[0].url
     }
   }
 </script>
