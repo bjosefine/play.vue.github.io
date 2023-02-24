a89165615493045f4a3e81de78e449b6ba2d1016
StinaNorqvist <norqvist.stina@gmail.com>
2023-02-24 17:26:17 +0100

    PlayerView styling

--- src/views/PlayerView.vue
+++ src/views/PlayerView.vue
@@ -1,29 +1,60 @@
 <template>
+  <!-- Background blurry image -->
   <div
     id="songBG"
     :style="{
       backgroundImage: 'url(' + img + ')',
-      filter: 'blur(4px)'
+      filter: 'blur(10px)'
     }"
   ></div>
-  <p>songs</p>
-  <div v-if="tracks">
-    <div>
-      {{ tracks.name }}
-      <img :src="tracks.album.images[0].url" alt="bild" style="width: 50%" />
-      <audio controls>
-        <source :src="tracks.preview_url" type="audio/mp3" />
-      </audio>
-    </div>
+
+  <!-- Player container with the page content -->
+  <div id="playerContainer">
+    <!-- <div v-if="tracks"> -->
+
+    <img
+      class="songImg"
+      :src="tracks.album.images[0].url"
+      alt="bild"
+      style="width: 50%"
+    />
+    <h1 class="songTitle">{{ tracks.name }}</h1>
+    <audio class="playerControl" controls>
+      <source class="playerMp3" :src="tracks.preview_url" type="audio/mp3" />
+    </audio>
+    <!-- </div>
+    <div v-else>Loading...</div> -->
   </div>
-  <div v-else>Loading...</div>
 </template>
 
 <style>
   #songBG {
     display: block;
     width: 100%;
-    height: 100%;
+    height: 200%;
+    background-size: cover;
+    margin: 0;
+    padding: 0;
+    z-index: -100;
+    position: absolute;
+  }
+
+  #playerContainer {
+    display: flex;
+    justify-content: center;
+    align-items: baseline;
+    width: 100%;
+    margin: 0;
+  }
+
+  .songTitle,
+  songImg,
+  playerControl,
+  playerMp3 {
+    background-color: blue;
+    margin: 20px;
+    display: flex;
+    align-items: row;
   }
 </style>
 
